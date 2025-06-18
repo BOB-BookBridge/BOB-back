015d98dd2bde347dd9240b2624d75e7e2d2d66f1
이현수 <znight1020@naver.com>
2025-06-19 00:55:42 +0900

    refactor: 레디스 비즈니스 로직 분리

--- src/main/java/com/bob/domain/member/service/port/MemberRedisPort.java
+++ src/main/java/com/bob/domain/member/service/port/MemberRedisPort.java
@@ -1,18 +1,8 @@
 package com.bob.domain.member.service.port;
 
-import java.util.Optional;
-
 public interface MemberRedisPort {
-
-  void saveVerified(String email, String value, int expireMinutes);
 
   boolean isVerified(String email);
 
   void deleteVerified(String email);
-
-  void saveCode(String email, String code, int expireMinutes);
-
-  Optional<String> getCode(String email);
-
-  void deleteCode(String email);
 }
