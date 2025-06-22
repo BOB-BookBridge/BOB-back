ed7bbe60fa3a30059739c0cff70a4446bc59b80f
znight1020 <znight1020@naver.com>
2025-06-22 11:20:43 +0900

    refactor: 이미지파일 이름 생성 시 uuid7 사용

--- src/main/java/com/bob/global/utils/image/ImageUtils.java
+++ src/main/java/com/bob/global/utils/image/ImageUtils.java
@@ -1,15 +1,21 @@
 package com.bob.global.utils.image;
 
+import static com.bob.global.utils.uuid.UuidUtils.randomV7;
+
 import com.bob.global.exception.exceptions.ApplicationException;
 import com.bob.global.exception.response.ApplicationError;
-import java.util.UUID;
+import java.util.List;
 
 public class ImageUtils {
 
   public static String generateImageFileName(String contentType, ImageDirectory directory) {
     String extension = extractExtension(contentType);
-    String uuid = UUID.randomUUID().toString();
+    String uuid = randomV7().toString();
     return directory.getPrefix() + uuid + "." + extension;
+  }
+
+  public static List<String> generateImageFileNames(List<String> contentTypes, ImageDirectory directory) {
+    return contentTypes.stream().map((type) -> generateImageFileName(type, directory)).toList();
   }
 
   public static String extractExtension(String contentType) {
