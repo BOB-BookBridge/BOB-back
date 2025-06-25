d17166d177e14699f00b4ea63b50be296b19592a
이현수 <znight1020@naver.com>
2025-06-26 01:00:55 +0900

    test: 파일 수정 기능 통합 테스트

--- src/test/intg/java/com/bob/domain/file/service/FileServiceIntgTest.java
+++ src/test/intg/java/com/bob/domain/file/service/FileServiceIntgTest.java
@@ -1,11 +1,16 @@
 package com.bob.domain.file.service;
 
+import static com.bob.global.exception.response.ApplicationError.FILE_UNAUTHORIZED;
 import static com.bob.global.utils.image.ImageDirectory.POST;
+import static com.bob.support.fixture.command.ChangeFileCommandFixture.DEFAULT_CHANGE_FILE_COMMAND_REF_ID_1;
+import static com.bob.support.fixture.command.ChangeFileCommandFixture.DEFAULT_CHANGE_FILE_COMMAND_REF_ID_2;
 import static com.bob.support.fixture.command.CreatePostCommandFixture.FILE_NAMES;
 import static com.bob.support.fixture.command.RegisterFileCommandFixture.defaultRegisterFileCommand;
 import static com.bob.support.fixture.domain.FileFixture.customRefIdFiles;
 import static com.bob.support.fixture.domain.FileFixture.defaultFiles;
+import static com.bob.support.fixture.domain.FileFixture.otherFiles;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 import com.bob.domain.file.entity.File;
@@ -18,6 +23,7 @@ import com.bob.domain.file.service.dto.response.FileUploadUrlResponse;
 import com.bob.domain.file.service.dto.response.FilesResponse;
 import com.bob.domain.file.service.port.FileImagePort;
 import com.bob.domain.file.service.reader.FileReader;
+import com.bob.global.exception.exceptions.ApplicationException;
 import com.bob.global.utils.image.ImageUtils;
 import com.bob.support.TestContainerSupport;
 import com.bob.support.redis.RedisContainerConfig;
@@ -89,6 +95,45 @@ class FileServiceIntgTest extends TestContainerSupport {
     assertThat(response.summaries())
         .extracting("fileName")
         .containsExactlyElementsOf(files.stream().map(File::getFileName).toList());
+  }
+
+  @DisplayName("파일 변경 - 성공 테스트")
+  @Test
+  void 파일을_변경할_수_있다() {
+    // given
+    String referenceId = "1";
+    List<File> oldFiles = defaultFiles();
+    fileRepository.saveAll(oldFiles);
+
+    // when
+    fileService.changeFileProcess(DEFAULT_CHANGE_FILE_COMMAND_REF_ID_1);
+
+    // then
+    List<File> newFiles = fileReader.readFileByReferenceId(referenceId);
+    assertThat(newFiles).hasSize(2);
+    assertThat(newFiles)
+        .extracting(File::getFileName)
+        .containsExactly("post/other1.png", "post/other2.jpg");
+    assertThat(newFiles)
+        .extracting(File::getSequence)
+        .containsExactly(0, 1);
+  }
+
+  @DisplayName("파일 변경 - 실패 테스트(권한 없음)")
+  @Test
+  void 파일을_수정할_때_파일을_올린_사람이_아닌_경우_예외가_발생한다() {
+    // given
+    String referenceId = "2";
+    List<File> files = otherFiles();
+    fileRepository.saveAll(files);
+
+    // when & then
+    assertThatThrownBy(() -> fileService.changeFileProcess(DEFAULT_CHANGE_FILE_COMMAND_REF_ID_2))
+        .isInstanceOf(ApplicationException.class)
+        .hasMessage(FILE_UNAUTHORIZED.getMessage());
+
+    List<File> result = fileReader.readFileByReferenceId(referenceId);
+    assertThat(result).hasSize(files.size());
   }
 
   @Test
