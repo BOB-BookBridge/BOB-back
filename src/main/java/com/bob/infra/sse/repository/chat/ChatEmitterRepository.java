04a2c89df71f6ba84b39ab281756d9e823f73d27
znight1020 <znight1020@naver.com>
2025-06-28 16:11:35 +0900

    feat: 알림, 채팅 emitter 저장소 구현

---
+++ src/main/java/com/bob/infra/sse/repository/chat/ChatEmitterRepository.java
@@ -0,0 +1,39 @@
+package com.bob.infra.sse.repository.chat;
+
+import com.bob.infra.sse.repository.EmitterRepository;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Component
+public class ChatEmitterRepository implements EmitterRepository<ChatEmitterKey> {
+
+  private final Map<ChatEmitterKey, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+  @Override
+  public SseEmitter save(ChatEmitterKey key, SseEmitter emitter) {
+    emitters.put(key, emitter);
+    return emitter;
+  }
+
+  @Override
+  public SseEmitter get(ChatEmitterKey key) {
+    return emitters.get(key);
+  }
+
+  @Override
+  public Map<ChatEmitterKey, SseEmitter> findAll() {
+    return emitters;
+  }
+
+  @Override
+  public void remove(ChatEmitterKey key) {
+    emitters.remove(key);
+  }
+
+  @Override
+  public boolean exists(ChatEmitterKey key) {
+    return emitters.containsKey(key);
+  }
+}
