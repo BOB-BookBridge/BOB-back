33d12f1b6b18b19addc12be41aa8fc1f3c7455a2
znight1020 <znight1020@naver.com>
2025-07-02 18:47:14 +0900

    feat: 참여중인 채팅방 목록 조회 기능 구현

--- src/main/java/com/bob/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/bob/domain/chat/repository/ChatRoomRepository.java
@@ -1,12 +1,21 @@
 package com.bob.domain.chat.repository;
 
 import com.bob.domain.chat.entity.ChatRoom;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChatRoomRepository extends CrudRepository<ChatRoom, Long> {
+
+  @Query("""
+        SELECT cr
+        FROM ChatRoom cr
+          JOIN ChatRoomMember crm ON cr.id = crm.chatRoomId
+        WHERE crm.memberId = :memberId
+      """)
+  List<ChatRoom> findAllByMemberId(UUID memberId);
 
   @Query("""
         SELECT crm.chatRoomId
--- src/main/java/com/bob/domain/chat/service/reader/ChatRoomReader.java
+++ src/main/java/com/bob/domain/chat/service/reader/ChatRoomReader.java
@@ -2,6 +2,7 @@ package com.bob.domain.chat.service.reader;
 
 import com.bob.domain.chat.entity.ChatRoom;
 import com.bob.domain.chat.repository.ChatRoomRepository;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatRoomReader {
 
   private final ChatRoomRepository chatRoomRepository;
+
+  public List<ChatRoom> readParticipatingChatRoomsByMemberId(UUID memberId) {
+    return chatRoomRepository.findAllByMemberId(memberId);
+  }
 
   public Optional<Long> readExistingChatRoom(Long postId, UUID sellerId, UUID buyerId) {
     return chatRoomRepository.findExistingChatRoom(postId, sellerId, buyerId);
