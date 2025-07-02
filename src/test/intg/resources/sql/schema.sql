941bb1898e8056194cdfa9b792b9b12ae25425e7
znight1020 <znight1020@naver.com>
2025-07-02 18:05:15 +0900

    feat: 채팅방 목록 조회 성능 개선을 위한 파생 컬럼 추가 - lastChatMessage

--- src/test/intg/resources/sql/schema.sql
+++ src/test/intg/resources/sql/schema.sql
@@ -149,8 +149,9 @@ CREATE TABLE IF NOT EXISTS chat_rooms (
     post_id BIGINT NOT NULL,
     trade_id BIGINT NOT NULL,
     title_suffix VARCHAR(100) NOT NULL,
+    last_chat_message VARCHAR(500),
+    last_chat_at DATETIME,
     enable_status BOOLEAN NOT NULL,
-    last_chat_at DATETIME NOT NULL,
     created_at DATETIME NOT NULL,
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (trade_id) REFERENCES trades(id)
