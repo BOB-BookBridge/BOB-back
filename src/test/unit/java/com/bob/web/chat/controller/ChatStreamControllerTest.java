a9f09b4fb8c41f573c7651d3f7104c0ccfe69fc9
이현수 <znight1020@naver.com>
2025-08-15 20:43:36 +0900

    test: 관련 테스트 수정

--- src/test/unit/java/com/bob/web/chat/controller/ChatStreamControllerTest.java
+++ src/test/unit/java/com/bob/web/chat/controller/ChatStreamControllerTest.java
@@ -13,10 +13,6 @@ import com.bob.domain.chat.usecase.ChatRoomReadUseCase;
 import com.bob.global.event.sse.manager.EmitterManager;
 import com.bob.global.exception.exceptions.ApplicationException;
 import com.bob.global.exception.response.ApplicationError;
-import com.bob.infra.auth.jwt.JwtProvider;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,34 +37,16 @@ class ChatStreamControllerTest {
   @Mock
   private EmitterManager emitterManager;
 
-  @Mock
-  private JwtProvider jwtProvider;
-
-  @Mock
-  private HttpServletRequest request;
-
-  private final String token = "mock.auth.token";
-
-  @BeforeEach
-  void setup() {
-    Cookie cookie = new Cookie("AUTHORIZATION", token);
-    given(request.getCookies()).willReturn(new Cookie[]{cookie});
-  }
-
   @Test
   @DisplayName("SSE 구독 - 성공 테스트")
   void 채팅방_SSE_구독에_성공한다() {
     // given
-    given(jwtProvider.isVerified(token)).willReturn(true);
-    given(jwtProvider.getMemberId(token)).willReturn(MEMBER_ID);
     given(emitterManager.subscribeToChat(1L, MEMBER_ID)).willReturn(new SseEmitter());
 
     // when
-    SseEmitter emitter = controller.handleSubscribeChat(1L, request);
+    SseEmitter emitter = controller.handleSubscribeChat(1L, MEMBER_ID);
 
     // then
-    then(jwtProvider).should().isVerified(token);
-    then(jwtProvider).should().getMemberId(token);
     then(readUseCase).should().validateParticipant(ValidateParticipantQuery.of(1L, MEMBER_ID));
     then(modifyUseCase).should().enterChatRoomProcess(EnterChatRoomCommand.of(1L, MEMBER_ID));
     then(emitterManager).should().subscribeToChat(1L, MEMBER_ID);
@@ -76,55 +54,18 @@ class ChatStreamControllerTest {
   }
 
   @Test
-  @DisplayName("SSE 구독 - 실패 테스트(토큰 없음)")
-  void 토큰이_없으면_예외가_발생한다() {
-    // given
-    given(request.getCookies()).willReturn(null);
-
-    // when & then
-    assertThatThrownBy(() -> controller.handleSubscribeChat(1L, request))
-        .isInstanceOf(ApplicationException.class)
-        .hasMessage(ApplicationError.AUTHENTICATION_FAILED.getMessage());
-
-    then(jwtProvider).shouldHaveNoInteractions();
-    then(readUseCase).shouldHaveNoInteractions();
-    then(emitterManager).shouldHaveNoInteractions();
-  }
-
-  @Test
-  @DisplayName("SSE 구독 - 실패 테스트(토큰 검증 실패)")
-  void 인증되지_않은_토큰이면_예외가_발생한다() {
-    // given
-    given(jwtProvider.isVerified(token)).willReturn(false);
-
-    // when & then
-    assertThatThrownBy(() -> controller.handleSubscribeChat(1L, request))
-        .isInstanceOf(ApplicationException.class)
-        .hasMessage(ApplicationError.AUTHENTICATION_FAILED.getMessage());
-
-    then(jwtProvider).should().isVerified(token);
-    then(jwtProvider).shouldHaveNoMoreInteractions();
-    then(readUseCase).shouldHaveNoInteractions();
-    then(emitterManager).shouldHaveNoInteractions();
-  }
-
-  @Test
   @DisplayName("SSE 구독 - 실패 테스트(채팅방 참가자가 아님)")
   void 채팅방_참가자가_아니면_예외가_발생한다() {
     // given
-    given(jwtProvider.isVerified(token)).willReturn(true);
-    given(jwtProvider.getMemberId(token)).willReturn(MEMBER_ID);
     willThrow(new ApplicationException(ApplicationError.NOT_PARTICIPATED_CHAT_ROOM))
         .given(readUseCase)
         .validateParticipant(ValidateParticipantQuery.of(1L, MEMBER_ID));
 
     // when & then
-    assertThatThrownBy(() -> controller.handleSubscribeChat(1L, request))
+    assertThatThrownBy(() -> controller.handleSubscribeChat(1L, MEMBER_ID))
         .isInstanceOf(ApplicationException.class)
         .hasMessage(ApplicationError.NOT_PARTICIPATED_CHAT_ROOM.getMessage());
 
-    then(jwtProvider).should().isVerified(token);
-    then(jwtProvider).should().getMemberId(token);
     then(readUseCase).should().validateParticipant(ValidateParticipantQuery.of(1L, MEMBER_ID));
     then(emitterManager).shouldHaveNoInteractions();
   }
