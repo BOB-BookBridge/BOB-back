c41636b0c35dd2f1e7951679e7806ff0138020b0
이현수 <znight1020@naver.com>
2025-09-25 02:51:19 +0900

    test: 회원 도서 등록 통합 테스트

--- src/test/intg/resources/sql/schema.sql
+++ src/test/intg/resources/sql/schema.sql
@@ -26,14 +26,17 @@ CREATE TABLE IF NOT EXISTS members (
     created_at DATETIME
 );
 
-CREATE TABLE member_books (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+-- ========================
+-- MEMBER BOOKS TABLE
+-- ========================
+CREATE TABLE IF NOT EXISTS member_books (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     member_id BINARY(16) NOT NULL,
     book_id BIGINT NOT NULL,
-    book_status ENUM('BEST', 'HIGH', 'MEDIUM', 'LOW') NOT NULL,
+    status ENUM('BEST', 'HIGH', 'MEDIUM', 'LOW') NOT NULL,
     CONSTRAINT fk_mb_member FOREIGN KEY (member_id)  REFERENCES members(id) ON DELETE CASCADE,
     CONSTRAINT fk_mb_book FOREIGN KEY (book_id) REFERENCES books(id)
-)
+);
 
 -- ========================
 -- INTERESTS TABLE
