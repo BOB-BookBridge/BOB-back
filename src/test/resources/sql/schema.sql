9c8deae59b3df181782a9d2220e98e743ddca855
znight1020 <znight1020@naver.com>
2025-12-10 16:23:33 +0900

    test: 로그아웃 테스트 개선

--- src/test/resources/sql/schema.sql
+++ src/test/resources/sql/schema.sql
@@ -362,9 +362,9 @@ INSERT INTO member_areas (id, emd_id, authenticated_at) VALUES
 
 -- 회원
 INSERT INTO members (id, email, password, nickname, area_id, status, role) VALUES
-(UUID_TO_BIN('0199f8c2-30ed-7ee3-a757-16196412518c'), 'test@test.com', '{bcrypt}$2a$10$e9yC6oOgw6QJA/XalvPlcOUkjTfuqfqxRH1TECCdUrgNHGiB/RoCa', 'tester', 1, 'ACTIVE', 'USER'),
-(UUID_TO_BIN('019a6928-6f73-79f7-bd1b-6bfd4771a302'), 'other@test.com', '{bcrypt}$2a$10$e9yC6oOgw6QJA/XalvPlcOUkjTfuqfqxRH1TECCdUrgNHGiB/RoCa', 'other', 2, 'ACTIVE', 'USER'),
-(UUID_TO_BIN('019a6928-6f73-79f7-bd1b-6bfd4771a303'), 'mock@test.com', '{bcrypt}$2a$10$e9yC6oOgw6QJA/XalvPlcOUkjTfuqfqxRH1TECCdUrgNHGiB/RoCa', 'mock', 3, 'ACTIVE', 'USER');
+(UUID_TO_BIN('0199f8c2-30ed-7ee3-a757-16196412518c'), 'test@test.com', '{bcrypt}$2a$10$0wsmn7K4OYal.QNhW5.26.nmNROkJ4utkzCPjgzZS.sPyLnjVx5sa', 'tester', 1, 'ACTIVE', 'USER'),
+(UUID_TO_BIN('019a6928-6f73-79f7-bd1b-6bfd4771a302'), 'other@test.com', '{bcrypt}$2a$10$0wsmn7K4OYal.QNhW5.26.nmNROkJ4utkzCPjgzZS.sPyLnjVx5sa', 'other', 2, 'ACTIVE', 'USER'),
+(UUID_TO_BIN('019a6928-6f73-79f7-bd1b-6bfd4771a303'), 'mock@test.com', '{bcrypt}$2a$10$0wsmn7K4OYal.QNhW5.26.nmNROkJ4utkzCPjgzZS.sPyLnjVx5sa', 'mock', 3, 'ACTIVE', 'USER');
 
 INSERT INTO member_wishes (id, member_id, book_id) VALUES
 (1, UUID_TO_BIN('019a6928-6f73-79f7-bd1b-6bfd4771a302'), 2);
